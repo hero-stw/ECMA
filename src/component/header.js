258c81a40936ee50566e6967d34f5ab4ce171efe
Hero <16040582@vnu.edu.vn>
2022-01-12 11:39:58 +0700

    Update logo

--- src/component/header.js
+++ src/component/header.js
@@ -2,7 +2,7 @@ const Heading = {
     render() {
         return /*html*/`
             <div class="bg-blue-500 max-h-[100px] max-w-full " style="background-color: #2A3659; padding: 1rem">
-                <img src="./src/img/icon2a.png" width="150px"/>
+                <img src="https://phothongcaodang.fpt.edu.vn/wp-content/uploads/LOGO-PTC%C4%90-White-Transparency.png" width="150px"/>
             </div>
         `
     }
